107248f5143ff64c1abf4d53a76a0a262e06b323
Alheekmah Library <53709203+alheekmahlib@users.noreply.github.com>
2023-12-06 22:55:00 -0800

    Edit bottom Navigation Bar

--- lib/presentation/screens/home/widgets/daily_hadith.dart
+++ lib/presentation/screens/home/widgets/daily_hadith.dart
@@ -11,81 +11,84 @@ class DailyHadith extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
-    return beigeContainer(
-      context,
-      Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 8.0),
-            child: Text(
-              '| ${'dailyHadith'.tr} |',
-              style: TextStyle(
-                fontSize: 16.0,
-                fontFamily: 'kufi',
-                fontWeight: FontWeight.w500,
-                color: context.textDarkColor,
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: beigeContainer(
+        context,
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                '| ${'dailyHadith'.tr} |',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'kufi',
+                  fontWeight: FontWeight.w500,
+                  color: context.textDarkColor,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
             ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 8,
-                child: Container(
-                  width: width,
-                  margin: const EdgeInsets.all(8),
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 8.0),
-                  decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.background,
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(8.0),
-                      )),
+            Row(
+              children: [
+                Expanded(
+                  flex: 8,
+                  child: Container(
+                    width: width,
+                    margin: const EdgeInsets.all(8),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16.0, horizontal: 8.0),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.background,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(8.0),
+                        )),
+                    child: Text(
+                      '‏ "‏ الطُّهُورُ شَطْرُ الإِيمَانِ وَالْحَمْدُ لِلَّهِ تَمْلأُ الْمِيزَانَ ‏.‏ وَسُبْحَانَ اللَّهِ وَالْحَمْدُ لِلَّهِ تَمْلآنِ - أَوْ تَمْلأُ - مَا بَيْنَ السَّمَوَاتِ وَالأَرْضِ وَالصَّلاَةُ نُورٌ وَالصَّدَقَةُ بُرْهَانٌ وَالصَّبْرُ ضِيَاءٌ وَالْقُرْآنُ حُجَّةٌ لَكَ أَوْ عَلَيْكَ كُلُّ النَّاسِ يَغْدُو فَبَائِعٌ نَفْسَهُ فَمُعْتِقُهَا أَوْ مُوبِقُهَا ‏"',
+                      style: TextStyle(
+                          fontSize: 22.0,
+                          fontFamily: 'naskh',
+                          fontWeight: FontWeight.w500,
+                          color: context.textDarkColor,
+                          height: 1.5),
+                      textAlign: TextAlign.justify,
+                      textDirection: TextDirection.rtl,
+                    ),
+                  ),
+                ),
+                Expanded(
+                    flex: 2,
+                    child: book_name(context, '1', context.textDarkColor,
+                        height: 60)),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 7,
                   child: Text(
-                    '‏ "‏ الطُّهُورُ شَطْرُ الإِيمَانِ وَالْحَمْدُ لِلَّهِ تَمْلأُ الْمِيزَانَ ‏.‏ وَسُبْحَانَ اللَّهِ وَالْحَمْدُ لِلَّهِ تَمْلآنِ - أَوْ تَمْلأُ - مَا بَيْنَ السَّمَوَاتِ وَالأَرْضِ وَالصَّلاَةُ نُورٌ وَالصَّدَقَةُ بُرْهَانٌ وَالصَّبْرُ ضِيَاءٌ وَالْقُرْآنُ حُجَّةٌ لَكَ أَوْ عَلَيْكَ كُلُّ النَّاسِ يَغْدُو فَبَائِعٌ نَفْسَهُ فَمُعْتِقُهَا أَوْ مُوبِقُهَا ‏"',
+                    'كتاب الطهارة',
                     style: TextStyle(
-                        fontSize: 22.0,
-                        fontFamily: 'naskh',
-                        fontWeight: FontWeight.w500,
-                        color: context.textDarkColor,
-                        height: 1.5),
+                      fontSize: 16.0,
+                      fontFamily: 'kufi',
+                      fontWeight: FontWeight.w600,
+                      color: context.textDarkColor,
+                    ),
                     textAlign: TextAlign.justify,
                     textDirection: TextDirection.rtl,
                   ),
                 ),
-              ),
-              Expanded(
-                  flex: 2,
-                  child: book_name(context, '1', context.textDarkColor,
-                      height: 60)),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 7,
-                child: Text(
-                  'كتاب الطهارة',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontFamily: 'kufi',
-                    fontWeight: FontWeight.w600,
-                    color: context.textDarkColor,
-                  ),
-                  textAlign: TextAlign.justify,
-                  textDirection: TextDirection.rtl,
-                ),
-              ),
-              Expanded(
-                  flex: 2,
-                  child: Transform.translate(
-                    offset: const Offset(0, -5),
-                    child: hours(context, height: 25),
-                  )),
-            ],
-          )
-        ],
+                Expanded(
+                    flex: 2,
+                    child: Transform.translate(
+                      offset: const Offset(0, -5),
+                      child: hours(context, height: 25),
+                    )),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
